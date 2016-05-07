c724fb853230cfb2e6a245fb285ecd70c0913ead
Giorgi <gkink12@freeuni.edu.ge>
2016-05-08 03:42:43 +0400

    HelloContinuousIntegration added

---
+++ src/main/java/HelloContinuousIntegration.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Giorgi on 5/8/2016.
+ */
+public class HelloContinuousIntegration {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Continuous Integration");
+    }
+}
